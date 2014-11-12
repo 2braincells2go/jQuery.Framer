f0d0adc9374e46d2faeffc04e1ebd2ced7b5512c
eternity <eternity.hiro@gmail.com>
2014-11-13 07:51:00 +0900

    spin.jsのアップデート。pushState機能改善

--- js/sample.js
+++ js/sample.js
@@ -1,10 +1,23 @@
 $(function(){
 	$('.framer').Framer();
 
-	$('.ps_framer').Framer({
-		isPushState: true
-	});
+	// $('.ps_framer').Framer({
+	// 	isPushState: true
+	// });
 	_V_.options.flash.swf = "./js/videojs/video-js.swf";
 
 	prettyPrint();
+
+	$("#api_sample").on("click", function() {
+		$("body").append('<div id="api_contents">TESTEST <a href="#" id="api_close">CLOSE</a></div>');
+		$.Framer.open('#api_contents', 'inline');
+
+		$(document).on("click", '#api_close', function() {
+			$.Framer.close();
+
+			return false;
+		});
+
+		return false;
+	});
 });
